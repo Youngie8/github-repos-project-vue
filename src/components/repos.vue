61598549bb92839e65a94a997742d48c61f8f978
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-06-08 12:28:54 +0100

    installed vue-router, axios

--- src/components/repos.vue
+++ src/components/repos.vue
@@ -1,5 +1,33 @@
 <template>
-    <h1>
-        hello
-    </h1>
+    <h1>Oluwole-young's Repositories</h1>
+
+    <ul>
+        <RouterLink to="/" v-for="repo in repos">
+            <h3>
+                {{ repo.name }}
+            </h3>
+            <p>{{ repo.description? repo.description : "no descrption" }}</p>
+        </RouterLink>
+    </ul>
 </template>
+
+<script>
+    import { RouterLink } from 'vue-router';
+    import axios from 'axios'
+
+    export default {
+  data() {
+    return {
+      repos: []
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get('https://api.github.com/users/youngie8/repos');
+      this.repos = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+</script>
